7c17e7cd963060a7919c47725a8133a042a204d4
xfy9326 <xfy9326@qq.com>
2023-08-11 02:05:56 +0800

    Change package name

--- sync.sh
+++ sync.sh
@@ -4,8 +4,10 @@ CACHE_DIR=".git_cache"
 OPEN_JDK_GIT="https://github.com/openjdk/jdk.git"
 TARGET_BRANCH="master"
 TARGET_SUB_DIR="src/jdk.jdeps/share/classes/com/sun/tools/classfile/"
-INPUT_DIR="src/jdk.jdeps/share/classes"
+INPUT_DIR="src/jdk.jdeps/share/classes/com/sun/tools/classfile/"
 OUTPUT_DIR="src"
+OUTPUT_PACKAGE_NAME="openjdk.classfile"
+OUTPUT_PACKAGE_DIR="$OUTPUT_DIR/openjdk/classfile/"
 
 
 if [ ! -d "$CACHE_DIR" ]; then
@@ -24,8 +26,14 @@ if [ -d "$OUTPUT_DIR" ]; then
   rm -rf "$OUTPUT_DIR"
 fi
 
-cp -r "$CACHE_DIR/$INPUT_DIR" "$OUTPUT_DIR"
+mkdir -p "$OUTPUT_PACKAGE_DIR"
+
+cp -r "$CACHE_DIR/$INPUT_DIR"* "$OUTPUT_PACKAGE_DIR"
 
 if [ -f "$OUTPUT_DIR/module-info.java" ]; then
   rm -rf "$OUTPUT_DIR/module-info.java"
 fi
+
+PKG_PATTERN="com\.sun\.tools\.classfile"
+PKG_REPLACEMENT="$OUTPUT_PACKAGE_NAME"
+find "$OUTPUT_DIR" -type f -name "*.java" | xargs sed -r -i "s/$PKG_PATTERN/$PKG_REPLACEMENT/g"
